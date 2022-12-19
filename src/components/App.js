0705a9eeb10452df4454a9e3b2d1ee8ded0796e8
Vladyslav <vkadyrov@protonmail.com>
2022-12-19 13:04:36 -0500

    commit tree 20f45f0f5adac1fcc4217844aa56eb4f1a44ed2c

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Actors from './Actors';
+import Directors from './Directors';
+import Movies from './Movies';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/movies'>
+          <Movies />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/directors'>
+          <Directors />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/actors'>
+          <Actors />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { movies } from "../data";
+import React from 'react';
+import { movies } from '../data';
+
+const displayMovie = movies.map((movie) => {
+  return (
+    <div key={movie.time}>
+      <h2>{movie.title}</h2>
+      <p>Running time: {movie.time}</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+});
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovie}
+    </div>
+  );
 }
 
 export default Movies;
